a773c60b0bd7b7e59211ee99bad1139818177afd
5kdn <sakuden542+github@gmail.com>
2025-08-14 18:20:00 +0900

    feat: add F-5E Single
    注）未完成です。F-5Eシングルミッション日本語化です。翻訳ヘルプいただきたいのでVer 0.8です。
    
    original: https://uu.getuploader.com/DCSJP1/download/47
    2018/07/08 11:00:58
    
    ---
    注意！！！　　現在の翻訳は誤訳が含まれている可能性が高いです。あくまでver.0.8と了承の上ご使用ください。
    
    ＊お願い＊
    翻訳についてですが、基本的に原文直訳風味です。好みに合わない方はご使用をご遠慮ください。
    また、標準的な用語でも、あえて省略せずまわりくどい言い方になっています。
    ですので、ショートカットキー操作を含めて、御自身で不要な部分だと思う箇所ははSAKURAエディタなどを使って、
    削ってカスタマイズしていただくことを前提としています。削るほうが、調べるよりは手間取らないと思いますので。
    
    あくまで素人が行った翻訳ですので、誤訳があった際には本スレへフィードバックを何卒御協力お願いいたします。
    
    /////////////////////////////////////導入手順/////////////////////////////////////////////////////////
    
    １）　うまくいかなかった時の復旧用に、バックアップを確保しながらの作業手順を記載します。
    
    　　　　○インストールフォルダ\Mods\aircraft\F-5E\Missions\Single　内
    　　　　　●オリジナルの「localization.lua」をコピーして適宜名前に変更してバックアップしたら
    　　　　　　添付の「localization.lua」と差し替え
    
    　　　　　●「F-5E Mission - Hercules.miz」をコピーして、コピーしたファイルを
    　　　　　　「jp-F-5E Mission - Hercules.miz」にリネーム　（要はjp-を頭につけるだけ）　　　　　
              ・上記のようにSingle内の、他の全て.mizを処理する。
    
    　　　　○インストールフォルダ￥config　内
    　　　　　・「lang.cfg」をコピーして適宜名前に変更してバックアップしたら
    　　　　　　添付の「lang.cfg」と差し替え
    
    　　
    ２）　・解凍ソフト（Explzh推奨）を使って処理してください。
    　　　　インストールフォルダ\Mods\aircraft\F-5E\Missions\Single　内の
    　　　　　　先ほどリネームした「jp-F-5E Mission - Hercules.miz」を
    　　　　　　　Explzhにドラッグして展開状態にして
    　　　　　　　添付のF-5E Mission - Herculesフォルダ内の
    　　　　　　　「l10nフォルダ」を上書き処理して保存。
    
    　　　　　　　その他.mizも同様に処理する。
    
    ////////////////////以上です。///////////////////////////////////
    
    更新履歴
    2018/7/8 ver.0.8   AWACSハント　Hercules　Airbase 校正(ﾜｯﾁｮｲ 674c-KMj/)さん

---
+++ DCSWorld/Mods/aircraft/F-5E/Missions/Single/localization.lua
@@ -0,0 +1,8 @@
+LocalizationTbl = {
+["F-5E Mission - Airbase.miz"] = _("エアベース　ディフェンス"),
+["F-5E Mission - AWACS.miz"] = _("AWACSハント"),
+["F-5E Mission - Hercules.miz"] = _("C-130 ハーキュリーズ"),
+["F-5E Mission - Ships.miz"] = _("敵艦隊攻撃"),
+}
+
+
