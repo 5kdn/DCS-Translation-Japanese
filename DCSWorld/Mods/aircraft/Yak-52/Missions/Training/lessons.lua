6529dff837b8096d625bfc99ae4f79ae1048790f
5kdn <sakuden542+github@gmail.com>
2025-11-28 02:41:43 +0900

    feat: add Yak-52 Training Mission (#23)
    original: https://uu.getuploader.com/DCSJP1/download/88
    2018/11/23 16:21:28
    
    ---
    
    日本語化の方法は別にアップしていますのでそれを参考にして下さい。
    
    パーキングブレーキの設定がありませんので、
    キー設定からWheel_Brakes_lockにU（初期値）を割り当ててくだし。ブレーキ軸を使う方は不要です。
    スタートアップとシャットダウンに必要になります。
    パーキングブレーキのかけ方はWを押したままブレーキをMAXにしてからUを押して、Uを離す前にWを離します。
    
    スタートアップでは地上員にチョークを置くように指示してもOKです。

---
+++ DCSWorld/Mods/aircraft/Yak-52/Missions/Training/lessons.lua
@@ -0,0 +1,18 @@
+lessons = 
+{
+	{
+        ["file"] = "yak_startup.miz",
+        ["name"] = _("エンジン始動手順"),
+        ["description"] = _("このレッスンでは駐機場でのYak-52の始動手順を学習します。"),
+    },
+	{
+        ["file"] = "yak_takeoff.miz",
+        ["name"] = _("タキシングと離陸"),
+        ["description"] = _("このレッスンでは、Yak-52の滑走路までの移動と離陸を学習します。"),
+    },
+	{
+        ["file"] = "yak_landing.miz",
+        ["name"] = _("着陸"),
+        ["description"] = _("このレッスンでは、Yak-52の有視界着陸の練習と、機体のシャットダウンを学習します。"),
+    }, 
+} -- end of lessons
