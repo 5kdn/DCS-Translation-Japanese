00f11c8704ff348ec382122075d2fe1d5483d08f
5kdn <sakuden542+github@gmail.com>
2025-11-27 22:49:23 +0900

    feat: add Su-25T QuickStart (#14)
    original: https:uu.getuploader.com/SU25T/download/27
    2020/04/27 22:08:20

---
+++ DCSWorld/Mods/aircraft/Su-25T/Missions/QuickStart/quickstart.lua
@@ -0,0 +1,146 @@
+planes = {
+
+-- Caucasus
+	{
+    name = _('コールド＆ダーク'),
+    file = 'Su-25T - Caucasus - Cold and Dark.miz',
+    },
+	{
+    name = _('離陸'),
+    file = 'Su-25T - Caucasus - Takeoff.miz',
+    },
+	{
+    name = _('ゲレンジークでのフリーフライト'),
+    file = 'Su-25T - Caucasus - Free Flight West.miz',
+    },
+	{
+    name = _('ミネラーリヌィエ・ヴォードィでのフリーフライト'),
+    file = 'Su-25T - Caucasus - Free Flight East.miz',
+    },
+	{
+    name = _('クラスノダールでの夜間フリーフライト'),
+    file = 'Su-25T - Caucasus - Night Free Flight.miz',
+    },
+	{
+    name = _('クラスノダールへの着陸'),
+    file = 'Su-25T - Caucasus - Landing.miz',
+    },
+	{
+    name = _('モズドクへのダストランディング'),
+    file = 'Su-25T - Caucasus - Landing at Dusk.miz',
+    },
+	{
+    name = _('クラスノダールへの夜間着陸'),
+    file = 'Su-25T - Caucasus - Night Landing.miz',
+    },
+	{
+    name = _('1 vs 1 攻撃的基本戦闘機機動'),
+    file = 'Su-25T - Caucasus - 1v1 OBFM.miz',
+    },
+	{
+    name = _('2 vs 1 空戦機動'),
+    file = 'Su-25T - Caucasus - 2v1 ACM.miz',
+    },
+	{
+    name = _('ドッグファイト！'),
+    file = 'Su-25T - Caucasus - 1v2 Dogfight.miz',
+    },
+	{
+    name = _('ターゲット訓練'),
+    file = 'Su-25T - Caucasus - Target Practice.miz',
+    },
+	{
+    name = _('近接航空支援 - 簡単'),
+    file = 'Su-25T - Caucasus - CAS Easy.miz',
+    },
+	{
+    name = _('近接航空支援 - 中難度'),
+    file = 'Su-25T - Caucasus - CAS Medium.miz',
+    },
+	{
+    name = _('近接航空支援 - 高難度'),
+    file = 'Su-25T - Caucasus - CAS Difficult.miz',
+    },
+	{
+    name = _('地上攻撃'),
+    file = 'Su-25T - Caucasus - Ground Attack.miz',
+    },
+	{
+    name = _('SEAD'),
+    file = 'Su-25T - Caucasus - SEAD.miz',
+    },
+
+-- Nevada
+	{
+    name = _('コールドスタート'),
+    file = 'Su-25T - Nevada - Cold Start.miz',
+    },
+	{
+    name = _('離陸'),
+    file = 'Su-25T - Nevada - Takeoff.miz',
+    },
+	{
+    name = _('ネバダ　フリーフライト'),
+    file = 'Su-25T - Nevada - Free Flight.miz',
+    },
+    {
+    name = _('ターゲット訓練'),
+    file = 'Su-25T - Nevada - Target Practice.miz',
+    },
+
+-- Normandy
+	{
+    name = _('離陸'),
+    file = 'Su-25T - Normandy - Takeoff.miz',
+    },
+	{
+    name = _('フリーフライト'),
+    file = 'Su-25T - Normandy - Free Flight.miz',
+    },
+	{
+    name = _('着陸'),
+    file = 'Su-25T - Normandy - Landing.miz',
+    },
+	{
+    name = _('兵器訓練'),
+    file = 'Su-25T - Normandy - Weapons Practice.miz',
+    },
+	{
+    name = _('近接航空支援'),
+    file = 'Su-25T - Normandy - Close Air Support.miz',
+    },
+	{
+    name = _('空対空'),
+    file = 'Su-25T - Normandy - Air to Air.miz',
+    },
+
+-- Persian Gulf
+	{
+    name = _('コールドスタート'),
+    file = 'Su-25T - Persian Gulf - Cold Start.miz',
+    },
+	{
+    name = _('離陸'),
+    file = 'Su-25T - Persian Gulf - Takeoff.miz',
+    },
+	{
+    name = _('フリーフライト'),
+    file = 'Su-25T - Persian Gulf - Free Flight.miz',
+    },
+	{
+    name = _('着陸'),
+    file = 'Su-25T - Persian Gulf - Landing.miz',
+    },
+	{
+    name = _('兵器訓練'),
+    file = 'Su-25T - Persian Gulf - Weapons Practice.miz',
+    },
+	{
+    name = _('ストライク'),
+    file = 'Su-25T - Persian Gulf - Strike.miz',
+    },
+	{
+    name = _('空対空'),
+    file = 'Su-25T - Persian Gulf - Air to Air.miz',
+    },
+}
