0d8d45a1a67dbdb3203528bf30aeeb54ac31dd0d
5kdn <sakuden542+github@gmail.com>
2025-11-27 14:51:56 +0900

    feat: add F-5E Training
    original: https://uu.getuploader.com/DCSJP1/download/40
    2018/06/26 19:37:54
    翻訳ファイルのみ採用
    
    ---
    
    ＊お願い＊
    
    翻訳についてですが、基本的に原文直訳風味です。好みに合わない方はご使用をご遠慮ください。
    また、標準的な用語でも、あえて省略せずまわりくどい言い方になっています。
    ですので、ショートカットキー操作を含めて、御自身で不要な部分と思う箇所はSAKURAエディタ等テキストエディタを使って、
    削ってカスタマイズしていただくことを前提としています。削るほうが、調べるよりは手間取らないと思いますので。
    
    あくまで素人が行った翻訳ですので、誤訳等があった際には日本語化スレへフィードバックを何卒御協力お願いいたします。
    適宜ブラッシュアップしていきます。(現在Ver 3.2)
    
    /////////////////////////////////////導入手順/////////////////////////////////////////////////////////
    
    １）　うまくいかなかった時の復旧用に、バックアップを確保しながらの作業手順を記載します。
    
    　　　　○インストールフォルダ\Mods\aircraft\F-5E\Missions\Training　内
    　　　　　●オリジナルの「lesson.lua」をコピーして適宜名前に変更してバックアップしたら
    　　　　　　添付の「lesson.lua」と差し替え
    
    　　　　　●「F-5E Training - Cold Start - Taxi - Takeoff.miz」をコピーして、コピーしたファイルを
    　　　　　　「jp-F-5E Training - Cold Start - Taxi - Takeoff.miz」にリネーム　（要はjp-を頭につけるだけ）　　　　　
              ・上記のようにトレーニングフォルダ内の、他の全て.mizを処理する。
    
    　　　　○インストールフォルダ￥config　内
    　　　　　・「lang.cfg」をコピーして適宜名前に変更してバックアップしたら
    　　　　　　添付の「lang.cfg」と差し替え
    
    　　
    ２）　・解凍ソフト（Explzh推奨）を使って処理してください。
    　　　　○インストールフォルダ\Mods\aircraft\F-5E\Missions\Training　内
    　　　　　　先ほどリネームした「jp-F-5E Training - Cold Start - Taxi - Takeoff.miz」を
    　　　　　　　Explzhにドラッグして展開状態にして
    　　　　　　　添付のF-5E Training - Cold Start - Taxi - Takeoffフォルダ内の
    　　　　　　　「l10nフォルダ」を上書き処理して保存。
    
    　　　　　　　jp-F-5E Training - Landing以降の.mizも同様に処理する。
    
    ////////////////////以上です。///////////////////////////////////
    
    DCS World本体Ver1.58以外のユーザーは
    
    ３）　F-5E Training - A2G Guided bomb.mizのみ
    　　　　２）作業同様に展開して、添付のmissonファイルと差し替えてください。　
    
    注　　　A2G Guided bombはVer1.58以外のバニラのmissonファイルにバグがあり、JTACからの連絡がうまく来ないので
    　　　　必ず３）を行って添付のmissonファイルに差し替えてください。動作確認できています。(2018/6/24)
    　　　　　日本語化スレ Part.1　170にてﾜｯﾁｮｲ 9f4c-E6HKさんに感謝！
    
    更新履歴
    Ver 3.2 Landingの翻訳更新
    Ver 3.1 Navigationの翻訳更新
    Ver 3.0 Navigationの翻訳更新
    Ver 2.0 A2G Guided bombの翻訳更新と差し替え用missonファイル追加

---
+++ DCSWorld/Mods/aircraft/F-5E/Missions/Training/lessons.lua
@@ -0,0 +1,108 @@
+lessons =
+{
+	{
+        ["file"] = "F-5E Training - Cold Start - Taxi - Takeoff.miz",
+        ["name"] = _("F-5E コールドスタート、タキシングと離陸"),
+        ["description"] = _("F-5Eでのコールドスタートの手順を学び、タキシングと離陸の訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - Landing.miz",
+        ["name"] = _("F-5E アプローチと着陸"),
+        ["description"] = _("目視でのアプローチと着陸の訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2A MSL.miz",
+        ["name"] = _("F-5E 空対空ミサイルの使用(MSLモード)"),
+        ["description"] = _("AIM-9 空対空ミサイルのミサイルモードでの使用法の訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2A DM.miz",
+        ["name"] = _("F-5E 空対空ミサイルの使用(DMモード)"),
+        ["description"] = _("AIM-9 空対空ミサイルのドッグファイトミサイルモードでの使用法の訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2A DG - AA1.miz",
+        ["name"] = _("F-5E 航空ターゲットとの機銃による交戦 (DGモード及びA/A1 GUNSモード)"),
+        ["description"] = _("ドッグファイトガン(DG)モード及びA/A1 GUNSモードによる、機銃での空対空戦闘訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2A AA2.miz",
+        ["name"] = _("F-5E 航空ターゲットとの機銃による交戦 (A/A2 GUNSモード)"),
+        ["description"] = _("A/A2 GUNSモードによる、機銃での空対空戦闘訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Bombs.miz",
+        ["name"] = _("F-5E 自由落下爆弾"),
+        ["description"] = _("無誘導爆弾の使用訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Rockets and Guns.miz",
+        ["name"] = _("F-5E 無誘導ロケット弾と機銃地上攻撃"),
+        ["description"] = _("空対地無誘導ロケット弾の使用、及び機銃による地上攻撃訓練を行う。"),
+    },
+	{
+        ["file"] = "F-5E Training - Navigation.miz",
+        ["name"] = _("F-5E TACANナビゲーション飛行"),
+        ["description"] = _("電波航法の一つである戦術航法装置(TACAN)を使用したナビゲーションの使い方を学ぶ。"),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Guided bomb.miz",
+        ["name"] = _("F-5E レーザー誘導爆弾"),
+        ["description"] = _("JTACとの通信手順の習得、及び誘導爆弾の使用訓練を行う。"),
+	},
+	{
+        ["file"] = "",
+        ["name"] = _(""),
+        ["description"] = _(""),
+    },
+    {
+        ["file"] = "F-5E Training - Cold Start - Taxi - Takeoff.miz",
+        ["name"] = _("F-5E. Cold Start, Taxiing and Takeoff"),
+        ["description"] = _("Learn the procedures to cold start, taxi and takeoff."),
+    },
+	{
+        ["file"] = "F-5E Training - Landing.miz",
+        ["name"] = _("F-5E. Approach and Landing"),
+        ["description"] = _("Practice visual approach and landing."),
+    },
+	{
+        ["file"] = "F-5E Training - A2A MSL.miz",
+        ["name"] = _("F-5E. Air-to-Air Missile Employment in MSL mode"),
+        ["description"] = _("Practice use of the AIM-9 air-to-air missile in Missile (MSL) mode."),
+    },
+	{
+        ["file"] = "F-5E Training - A2A DM.miz",
+        ["name"] = _("F-5E. Air-to-Air Missile Employment in DM mode"),
+        ["description"] = _("Practice use of the AIM-9 air-to-air missile in Dogfight Missile (DM) mode."),
+    },
+	{
+        ["file"] = "F-5E Training - A2A DG - AA1.miz",
+        ["name"] = _("F-5E. Engaging Aerial Targets using DG and A/A1 GUNS modes"),
+        ["description"] = _("Practice air-to-air gunnery in the Dogfight Gun (DG) and A/A1 GUNS modes."),
+    },
+	{
+        ["file"] = "F-5E Training - A2A AA2.miz",
+        ["name"] = _("F-5E. Engaging Aerial Targets using A/A2 GUNS mode"),
+        ["description"] = _("Practice air-to-air gunnery in the A/A2 GUNS mode."),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Bombs.miz",
+        ["name"] = _("F-5E. Free-Fall Bombs"),
+        ["description"] = _("Practice use of unguided bombs."),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Rockets and Guns.miz",
+        ["name"] = _("F-5E. Unguided Rockets and Guns"),
+        ["description"] = _("Practice strafing ground targets using of unguided rockets and guns."),
+    },
+	{
+        ["file"] = "F-5E Training - Navigation.miz",
+        ["name"] = _("F-5E. Flight and Radio Navigation"),
+        ["description"] = _("Practice use of the TACAN radio navigation system."),
+    },
+	{
+        ["file"] = "F-5E Training - A2G Guided bomb.miz",
+        ["name"] = _("F-5E. Laser-guided bomb"),
+        ["description"] = _("Practice use of guided bombs."),
+	}
+} -- end of lessons
