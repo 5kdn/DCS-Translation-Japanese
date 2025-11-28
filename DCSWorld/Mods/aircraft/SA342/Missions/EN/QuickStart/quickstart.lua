0a6743c4ef16e150ec0e21cd44d99662afa25fcf
5kdn <sakuden542+github@gmail.com>
2025-11-28 13:44:09 +0900

    feat: add SA342M default missions (#30)
    original: https://uu.getuploader.com/DCSJP1/download/109
    2019/02/25 22:30:52

---
+++ DCSWorld/Mods/aircraft/SA342/Missions/EN/QuickStart/quickstart.lua
@@ -0,0 +1,34 @@
+planes = {
+	{ 
+    name = _('停止タンク'),
+    file = 'Tanks_static_en.miz',
+	},
+	{ 
+    name = _('移動トラック'),
+    file = 'Trucks_move_en.miz',
+    },
+ 	{ 
+    name = _('停止トラック'),
+    file = 'Trucks_static_en.miz',
+    },
+	{ 
+    name = _('ネバダ　- コールドスタート'),
+    file = 'NTTR SA342M Cold Start_en.miz',
+    },
+	{ 
+    name = _('ネバダ　- 停止トラック'),
+    file = 'NTTR trucks static_en.miz',
+    },
+	{ 
+    name = _('夜間の停止トラック'),
+    file = 'Trucks_static_night_en.miz',
+    },
+	{ 
+    name = _('飛行スキル向上'),
+    file = 'Improve your piloting skill_en.miz',
+    },
+	{ 
+    name = _('ネバダ　- 飛行スキル向上'),
+    file = 'NTTR improve your piloting skill_en.miz',
+    },
+}
---
+++ DCSWorld/Mods/aircraft/SA342/Missions/EN/Training/lessons.lua
@@ -0,0 +1,53 @@
+lessons =
+{
+	{
+        ["file"] = "Start-up_en.miz",
+        ["name"] = "コールドスタート",
+        ["description"] = "このレッスンではガゼルを駐機場から始動させる練習を行います。",
+	},
+	{
+        ["file"] = "Weapons_en.miz",
+        ["name"] = "兵器",
+        ["description"] = "このレッスンでは兵器システムの練習を行います。",
+	},
+	{
+        ["file"] = "ADF_en.miz",
+        ["name"] = "ADF システム",
+        ["description"] = "このレッスンでは、ガゼルのADFシステムの使い方を練習します。",
+	},
+	{
+        ["file"] = "NADIR_PP_BUT_en.miz",
+        ["name"] = "NADIR PP BUT",
+        ["description"] = "このレッスンでは現在地点をウェイポイントに登録する練習を行います。",
+	},
+	{
+        ["file"] = "NADIR_BUT_BUT_en.miz",
+        ["name"] = "NADIR BUT BUT",
+        ["description"] = "このレッスンでは、ウェイポイントを別のウェイポイントへコピーする方法を学習します。",
+	},
+	{
+        ["file"] = "NADIR_DATA_en.miz",
+        ["name"] = "NADIR 座標入力",
+        ["description"] = "このレッスンでは、ウェイポイントの座標の手動で入力する方法を学習します",
+	},
+	{
+        ["file"] = "NTTR Start-up_en.miz",
+        ["name"] = "コールドスタート",
+        ["description"] = "このレッスンではガゼルを駐機場から始動させる練習を行います。",
+	},
+	{
+        ["file"] = "NTTR Weapons_en.miz",
+        ["name"] = "兵器",
+        ["description"] = "このレッスンでは兵器システムの練習を行います。",
+	},
+	{
+        ["file"] = "NTTR NADIR_PP_BUT_en.miz",
+        ["name"] = "NADIR PP BUT",
+        ["description"] = "このレッスンでは現在地点をウェイポイントに登録する練習を行います。",
+	},
+	{
+        ["file"] = "NTTR NADIR_BUT_BUT_en.miz",
+        ["name"] = "NADIR BUT BUT",
+        ["description"] = "このレッスンでは、ウェイポイントを別のウェイポイントへコピーする方法を学習します。",
+	},
+}
