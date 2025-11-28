530350073c1a6ec68060aaf1904e4734048962cc
5kdn <sakuden542+github@gmail.com>
2025-11-28 16:45:34 +0900

    feat: add Mi-8MTV2 Training
    original: https://uu.getuploader.com/DCSJP1/download/91
    2018/12/31 02:20:04
    
    ---
    
    Mi-8MTV2のトレーニングミッションです。
    
    Mi-8MTV2-TR-Vertical Takeoff Demoはリプレイなのでリプレイフォルダに入れると見ることができますが、
    Ver2.5では途中でコケてうまく再現できません。使用不可と思ってください。
    
    兵器の練習に入る前に、ドップラーシステムの練習を行い、オートパイロットについて学習しておくほうが良いでしょう。
    計器が多く、操作している間に機体が明後日の方に飛んでいってしまいます。
    
    翻訳の仕方は、別ファイルでアップしていますのでそちらを参考にしてください。
    
    ---
    
    参考程度に
    UH-1は80時間ほど飛ばしそこそこ飛べるようになっていましたが、
    Mi-8ではボルテックスリングで落ちまくりました。
    
    離陸時のサイクリックがUH-1は左後ですが、Mi-8は右後です。
    
    グラウンド・エフェクトがUH-1よりも弱いように思います。UH-1は地面付近でリフトが結構ありますが、
    こっちはないので慎重に降りないとドスンと落ちてしまいます。
    
    計器が多く、しかも席をあっち行ったりこっち行ったりしなくてはなりませんのでキーバインドをしたほうが楽でしょう。
    まだ慣れていないこともあり、もっさり感がつよく思えますが、飛んでいて楽しい機体です。
    
    DCSのヘリは楽しいですね
    
    今キャンペーンを翻訳しながら遊んでいます。
    2018.12.31

---
+++ DCSWorld/Mods/aircraft/Mi-8MTV2/Missions/Training/lessons.lua
@@ -0,0 +1,28 @@
+lessons = 
+{
+    {
+        ["file"] = "MI-8MTV2-Training-Cold-Start.miz",
+        ["name"] = _("コールドスタート"),
+        ["description"] = _("このレッスンでは駐機場のMi-8MTV2を始動する練習を行います。"),
+    },
+  --  {
+  --      ["file"] = "Mi-8MTV2-TR-Vertical Takeoff Demo.trk",
+  --      ["name"] = _("垂直離陸のデモンストレーション"),
+  --      ["description"] = _("This is a short track demonstrating how to perform a vertical takeoff and forward acceleration in the air."),
+  --  },
+    {
+        ["file"] = "MI-8MTV2-Training-Weapons.miz",
+        ["name"] = _("兵器の使いかた"),
+        ["description"] = _("このレッスンでは、Mi-8MTV2の兵器ステムの扱い方について説明と練習を行います。"),
+    },
+	{
+        ["file"] = "Mi-8MTV2 Training - Doppler.miz",
+        ["name"] = _("DISS-15 ドップラーシステムの使いかた"),
+        ["description"] = _("このレッスンでは、Mi-8MTV2のDISS-15ドップラーシステムの説明と使い方の練習を行います。"),
+    },
+	{
+        ["file"] = "Mi-8MTV2 Training - ARC9.miz",
+        ["name"] = _("ARK-9 ADF セットの使いかた"),
+        ["description"] = _("このレッスンでは、Mi-8MTV2のARK-9 ADF（自動方向探知）の説明と使いかたの練習を行います。"),
+    },
+} -- end of lessons
