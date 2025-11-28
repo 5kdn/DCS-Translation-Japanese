795962b20d3b6215214ce5c99dfbf151c01edd5a
5kdn <sakuden542+github@gmail.com>
2025-11-28 12:54:00 +0900

    feat: add AJS-37 Training (#26)
    original: https://uu.getuploader.com/DCSJP2/download/12
    2021/10/11 22:36:21

---
+++ DCSWorld/Mods/aircraft/AJS37/Missions/Training/lessons.lua
@@ -0,0 +1,58 @@
+lessons = 
+{
+	{
+        ["file"] = "01 - AJS37 Training Cold Start.miz",
+        ["name"] = _("コールドスタート手順"),
+        ["description"] = _("このレッスンでは、AJS-37ビゲンにおけるコールドスタート手順を学びます。"),
+    },
+	{
+        ["file"] = "02 - AJS37 Training Taxi.miz",
+        ["name"] = _("タキシングと地上滑走手順"),
+        ["description"] = _("このレッスンでは、タキシング手順とスラストリバーサー(逆推力装置)の使い方を練習します。"),
+    },
+	{
+        ["file"] = "03 - AJS37 Training Take Off.miz",
+        ["name"] = _("離陸手順"),
+        ["description"] = _("このレッスンでは、正しい離陸の手順を練習します。"),
+    },
+	{
+        ["file"] = "04 - AJS37 Training Basic Navigation.miz",
+        ["name"] = _("基本的な航法飛行"),
+        ["description"] = _("このレッスンでは、AJS-37のウェイポイントシステムを使った航法の基本を学びます。"),
+    },
+	{
+        ["file"] = "05 - AJS37 Training visual landing.miz",
+        ["name"] = _("有視界アプローチ、着陸とシャットダウン"),
+        ["description"] = _("このレッスンでは、基本的な有視界アプローチ、着陸、シャットダウンの手順を学びます。"),
+    },
+	{
+        ["file"] = "06 - AJS37 Training ARAK.miz",
+        ["name"] = _("空対地：ARAK M70B ロケットポッド"),
+        ["description"] = _("このレッスンでは、ARAK M70B 無誘導ロケットポッドの使い方を学びます。"),
+    },			
+	{
+        ["file"] = "07 - AJS37 Training RB04E.miz",
+        ["name"] = _("空対艦：RB-04E対艦ミサイル"),
+        ["description"] = _("このレッスンでは、RB-04E対艦ミサイルを使った船舶への攻撃方法を学びます。"),
+    },	
+	{
+        ["file"] = "08 - AJS37 Training RB05.miz",
+        ["name"] = _("空対地：RB-05A空対地ミサイル"),
+        ["description"] = _("このレッスンでは、手動指令誘導のRB-05Aミサイルを空対地モードで使用する方法を学びます。"),
+    },
+	{
+        ["file"] = "09 - AJS37 Training sb71hdCCIP.miz",
+        ["name"] = _("空対地：CCIPモードにおけるSB M/71爆弾 (高抵抗) "),
+        ["description"] = _("このレッスンでは、CCIPモードにおけるM/71爆弾の使い方を学びます"),
+    },
+	{
+        ["file"] = "10 - AJS37 Training BK90.miz",
+        ["name"] = _("空対地：BK-90クラスター弾ディスペンサー"),
+        ["description"] = _("このレッスンでは、あらかじめプログラムされた目標に対するBK-90の使い方を学びます。"),
+    },
+	{
+        ["file"] = "11 - AJS37 Training RB75.miz",
+        ["name"] = _("空対地：RB75空対地ミサイル"),
+        ["description"] = _("このレッスンでは、RB75(マーベリック) 空対地光学照準ミサイルの使い方を学びます"),
+    },			
+} -- end of lessons
