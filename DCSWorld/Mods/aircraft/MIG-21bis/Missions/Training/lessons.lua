891654099f3401e095687e9d62a96d202e513cd5
5kdn <sakuden542+github@gmail.com>
2025-11-28 13:25:21 +0900

    feat: add MiG-21Bis Training
    original: https://uu.getuploader.com/DCSJP2/download/2
    2021/09/12 16:18:28

---
+++ DCSWorld/Mods/aircraft/MIG-21bis/Missions/Training/lessons.lua
@@ -0,0 +1,78 @@
+lessons = 
+{
+	{
+        ["file"] = 'startup.miz',
+        ["name"] = _('駐機スタート手順'),
+        ["description"] = _('Mig-21における駐機スタート手順を学びます。'),
+    },
+	{
+		["file"] = 'taxi.miz',
+		["name"] = _('地上滑走'),
+		["description"] = _('ペダルとディファレンシャルブレーキを使ったMiG-21Bisのタキシング方法を学びます。'),
+    },
+	{
+		["file"] = 'takeoff.miz',
+		["name"] = _('離陸'),
+		["description"] = _('MiG-21Bisの離陸手順と操縦方法を学びます。'),
+    },
+	{
+		["file"] = 'landing.miz',
+		["name"] = _('着陸'),
+		["description"] = _('MiG-21Bisの有視界下での着陸を学びます。'),
+    },
+	{
+		["file"] = 'radar.miz',
+		["name"] = _('レーダー操作'),
+		["description"] = _('RP-22SMAレーダーを操作するための基礎知識を学びます。'),
+    },
+	{
+		["file"] = 'AA_missiles.miz',
+		["name"] = _('空対空ミサイル'),
+		["description"] = _('空対空(AA)ミサイルの使い方を学びます。'),
+    },
+	{
+		["file"] = 'AA_guns.miz',
+		["name"] = _('空対空戦闘での機関砲'),
+		["description"] = _('空対空(AA)兵装モードでの機関砲の使い方を学びます'),
+    },
+    {
+		["file"] = 'AG_dumb.miz',
+		["name"] = _('爆撃'),
+		["description"] = _('爆弾を使って地上の目標を攻撃する方法を学びます。'),
+    },
+	{
+		["file"] = 'AG_rockets_guns.miz',
+		["name"] = _('空対地ロケットと機関砲'),
+		["description"] = _('機関砲やロケットで地上の目標を攻撃することを学びます。'),
+    },
+	{
+		["file"] = 'AG_Grom.miz',
+		["name"] = _('KH-66 Grom'),
+		["description"] = _('レーダー誘導式の空対地ミサイル KH-66 Gromの使用方法を学びます。'),
+    },
+	{
+		["file"] = 'RSBN.miz',
+		["name"] = _('RSBNの操作'),
+		["description"] = _('RSBN戦術ナビゲーション支援の使い方を学びます。'),
+    },
+	{
+		["file"] = 'ARC.miz',
+		["name"] = _('ARCの操作'),
+		["description"] = _('自動方向探知機(ラジオコンパス)の使い方を学びます。'),
+    },
+	{
+		["file"] = 'Radials_interception.miz',
+		["name"] = _('RSBNを使ったラジアル・インターセプト'),
+		["description"] = _('選択したRSBN局のラジアルをインターセプトする方法を学びます。'),
+    },
+	{
+		["file"] = 'PRMG.miz',
+		["name"] = _('PRMGアプローチでの着陸'),
+		["description"] = _('PRMGを使用した計器着陸アプローチの方法を学びます。'),
+    },
+    {
+		["file"] = 'advanced_RSBN_big_box.miz',
+		["name"] = _('RSBNでの「ビッグボックス」構築'),
+		["description"] = _('「ビッグボックス」着陸パターンの飛行を学びます。'),
+    },
+} -- end of lessons
