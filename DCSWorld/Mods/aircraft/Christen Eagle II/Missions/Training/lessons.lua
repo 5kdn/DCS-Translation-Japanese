a226a8a46d2e72d1a265e0ade30970215241f4fc
5kdn <sakuden542+github@gmail.com>
2025-11-28 16:02:32 +0900

    feat: add Christen Eagle II Single and QuickStart (#33)
    original: https://uu.getuploader.com/DCSJP1/download/94
    2019/01/28 22:05:51
    
    ---
    
    クリスティン・イーグルIIのトレーニングミッションをクイックミッションです。
    クイックミッションの方は検証をしていません。
    
    軽量で反応がクイックなので最初はまごつくかもしれません。
    着陸がうまくいかない人は（特にバウンドする人は）スロットルアイドル、降下率ほぼ0で地上すれすれで我慢してみてください。
    他の機体のように降下率300とかだとすぐに跳ねます。

---
+++ DCSWorld/Mods/aircraft/Christen Eagle II/Missions/Training/lessons.lua
@@ -0,0 +1,23 @@
+lessons = 
+{
+
+	{
+        ["file"] = "Christen Eagle II Cold-Start-Lesson.miz",
+        ["name"] = _("クリスティン・イーグルIIの紹介とエンジンスタート"),
+        ["description"] = _("このレッスンでは駐機場からのエンジンスタートアップの練習を行います。"),
+    },
+	{
+        ["file"] = "Christen Eagle II Taxi-Takeoff-Lesson.miz",
+        ["name"] = _("タキシングと離陸のレッスン"),
+        ["description"] = _("このレッスンでは、タキシングと離陸の練習を行います。"),
+    },
+	{
+        ["file"] = "Christen Eagle II Landing-Lesson.miz",
+        ["name"] = _("着陸とエンジンシャットダウンのレッスン"),
+        ["description"] = _("このレッスンでは、有視界での着陸とシャットダウンの手順について練習します。"),
+    },
+	
+	
+	
+	
+} -- end of lessons
