1217c6464262ba8fc14bb563bc1d7005fd2dd348
5kdn <sakuden542+github@gmail.com>
2025-11-30 00:44:43 +0900

    feat: add Black Shark2 Training (#62)
    original: https://uu.getuploader.com/DCSJP1/download/129
    2019/5/30  0:13:00
    
    ---
    
    日本語化の手順については別途ファイルがありますのでそれをダウンロードして下さい。
    
    KA-50は日本語のマニュアルも途中までですがありますので参考にしてください。
    （KA-50　Black Shaark　Wikiで手に入れてください。）
    
    ライトの表示関係がおかしく、ボタンなどのオンオフが分かりづらい状態になっています。
    このファイルは1.58でも使えると思いますので、1.58でやるほうが良いかもしれません。
    その他の対処方法
    https://forums.eagle.ru/showthread.php?t=219669
    ここからLast　Versionを押しててきて解答して
    
    C:\Users\<User Name>\Saved Games\DCSに\Liveriesフォルダを入れると
    option Spacial にDefault with cockpit fixとEnglish with cockpit fixの項目が追加されます。
    これでかなり見やすくなります。
    
    コクピットは英語表記にしておくほうがわかりやすいでしょう。

---
+++ DCSWorld/Mods/aircraft/Ka-50/Missions/Training/lessons.lua
@@ -0,0 +1,38 @@
+lessons = 
+{
+	{
+        ["file"] = "BS2_Cold_Start_Training.miz",
+        ["name"] = _("簡易コールドスタート"),
+        ["description"] = _("このレッスンではKa-50を駐機場からスタートさせます。"),
+    },
+	{
+        ["file"] = "BS2_En-Route_Navigation_Training.miz",
+        ["name"] = _("エンルートナビゲーショントレーニング"),
+        ["description"] = _("このレッスンでは、PVI-800とオートパイロットを使ってエンルートナビゲーションの練習を行います。"),
+    },
+	{
+        ["file"] = "BS2_ABRIS_AMMS_Training.miz",
+        ["name"] = _("ABRIS AMMS 概略"),
+        ["description"] = _("このレッスンではABRIS AMMSシステムの大まかな説明をします。"),
+    },
+	{
+        ["file"] = "BS2_ABRIS_Advanced_Training.miz",
+        ["name"] = _("ABRIS 上級トレーニング"),
+        ["description"] = _("このレッスンではABRISメイン動作モードの様々なサブモードについて学習します。"),
+    },
+	{
+        ["file"] = "BS2_Weapon Employment_Training.miz",
+        ["name"] = _("兵器の運用"),
+        ["description"] = _("このレッスンでは兵器システムの使い方について学習します。"),
+    },
+	{
+        ["file"] = "BS2_Countermeasure_training.miz",
+        ["name"] = _("カウンターメジャー練習"),
+        ["description"] = _("このレッスンではL-140レーザー警告システム、UV-26フレア射出装置、信号弾について学習します。"),
+    },
+    {
+        ["file"] = "BS2_Datalink_Wingmen_Training.miz",
+        ["name"] = _("データーリンクとウイングマン"),
+        ["description"] = _("このレッスンでは、ウイングマンへの命令の出し方と、データーリンクを使って目標の指定の仕方を学習します。"),
+    },
+} -- end of lessons
